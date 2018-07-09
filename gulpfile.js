ba954e6cd3cc1b8facc52169434b24d7a5568de8
Rolf Loges <loges@traffics.de>
2018-07-09 11:10:58 +0200

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ gulp.task('js-concat', function() {
     return gulp.src(
             [
                 './node_modules/jquery/dist/jquery.min.js',
+                './node_modules/popper.js/dist/umd/popper.min.js',
                 './node_modules/bootstrap/dist/js/bootstrap.min.js',
                 './src/js/*.js'
             ]
